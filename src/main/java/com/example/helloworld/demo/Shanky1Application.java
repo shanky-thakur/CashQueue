0ced4de6014e26a89fe9e168171d754938b0dda9
shanky <thakurdeepanshu420@gmail.com>
2024-06-10 21:06:29 +0530

    initial commit

---
+++ src/main/java/com/example/helloworld/demo/Shanky1Application.java
@@ -0,0 +1,12 @@
+package com.example.helloworld.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Shanky1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Shanky1Application.class, args);
+	}
+}
---
+++ src/test/java/com/example/helloworld/demo/Shanky1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworld.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shanky1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
